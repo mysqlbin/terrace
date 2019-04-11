1b82e373ce30d308716c126bd9368d8d6a555a8a
mysqlbin <2955468423@qq.com>
2019-04-11 09:45:19 +0800

    .

--- automatic/myapp/views.py
+++ automatic/myapp/views.py
@@ -135,7 +135,7 @@ ORDER BY
                 countnum = 10
 
             sqllist = parse_binlog_self(insname, binname, begintime, tbname, dbselected, countnum)
-            # return HttpResponse(sqllist)
+            return HttpResponse(sqllist)
             return render(request, 'binlog_rollback.html', locals())
     return render(request, 'binlog_rollback.html', locals())   #返回字典类型的局部变量： {'z': 1, 'arg': 4} ,用于传递多个变量给模板中不同的模块
 
