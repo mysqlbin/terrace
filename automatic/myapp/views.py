39d176ac78d793571e3e224e2886cca4a2bd6ee7
mysqlbin <2955468423@qq.com>
2019-04-16 09:41:12 +0800

    .

--- automatic/myapp/views.py
+++ automatic/myapp/views.py
@@ -113,18 +113,12 @@ ORDER BY
         elif 'parse_commit' in request.POST:
 
             binname    = request.POST['binary_list']
-
-
-            start_pos  = str(request.POST['start_pos'])
-            start_pos = int(start_pos)
-
-            # stop_pos = None
-            stop_pos   = str(request.POST['stop_pos'])
-            if stop_pos <= '0':
+            start_pos  = int(request.POST['start_pos'])
+            stop_pos   = request.POST['stop_pos']
+            if stop_pos == '0':
                 stop_pos = None
             else:
                 stop_pos = int(stop_pos)
-
             begin_time  = request.POST['begin_time']
             tbname     = request.POST['tbname']
             dbselected = request.POST['dblist']
