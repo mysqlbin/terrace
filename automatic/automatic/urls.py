29760587162a14398f6e9cca6a551814a81a8268
mysqlbin <2955468423@qq.com>
2019-04-17 17:33:23 +0800

    .

--- automatic/automatic/urls.py
+++ automatic/automatic/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views as myapp_view
-from salt import views as salt_views
 
 app_name = ''
 
