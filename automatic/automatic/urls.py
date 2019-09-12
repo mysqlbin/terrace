a12132927b177d1e5b95d107d3b9da14b524b538
mysqlbin <2955468423@qq.com>
2019-09-12 17:17:29 +0800

    .

--- automatic/automatic/urls.py
+++ automatic/automatic/urls.py
@@ -14,49 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myapp import views as myapp_view, instance, slowlog, binlog, polling, test
+from django.urls import path,include
 
 app_name = ''
 
 urlpatterns = [
 
-    path('index/', myapp_view.index, name='index'),
-
-
-    path('slow_query/', myapp_view.slow_query, name='slow_query'),
-    path('slowlog/slowquery_review/', slowlog.slowquery_review),
-    path('slowlog/slowquery_review_history/', slowlog.slowquery_review_history),
-
-
-    path('polling_report/', myapp_view.polling_report, name='polling_report'),
-    path('polling/polling_list/', polling.polling_list),
-    path('polling/get_polling_report/', polling.get_polling_report),
-
-
-    # path('login/', myapp_view.login, name='login'),
-
-    # path('metas/', myapp_view.metas, name='metas'),
-
-    path('binlog2sql/', myapp_view.binlog2sql, name='binlog2sql'),
-    path('binlog/binlog2sql/', binlog.binlog2sql),
-
-    path('instance/', myapp_view.instance, name='instance'),
-    path('instance/get_lists/', instance.get_lists),
-    path('instance/get_instances_resource/', instance.get_instances_resource),
-    path('instance/get_instance_name_id/', instance.get_instance_name_id),
-    path('instance/get_instance_users/<id>/<instance_name>/', instance.get_instance_users, name='users'),
-
-
-    path('instance/get_instances_binlog/', instance.get_instances_binlog),
-
-    path('test/get_query_data/', test.get_query_data),
-
-
-    #test
-    path('index/', myapp_view.index, name='index'),
-
-
     path('admin/', admin.site.urls),
-
+    path('', include('myapp.urls'))
 ]
