0ed29eab64d02251f450bc2b8b9af3af165ad814
mysqlbin <38709460+mysqlbin@users.noreply.github.com>
2019-09-21 22:20:51 +0800

    .

--- automatic/automatic/settings.py
+++ automatic/automatic/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',  #查找静态资源路径
     'myapp',
     'blacklist',
 ]
